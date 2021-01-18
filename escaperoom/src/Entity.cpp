b46ce5c686e8e9f5eba542182521de3c8efdc2eb
Máté Molnár <mmateka89@gmail.com>
2021-01-18 14:07:08 +0100

    Make Entity class's remove function search recursively

--- escaperoom/src/Entity.cpp
+++ escaperoom/src/Entity.cpp
@@ -10,9 +10,20 @@ void Entity::add(std::shared_ptr<Entity> e) {
 }
 
 std::shared_ptr<Entity> Entity::remove(const std::string& name) {
-	std::shared_ptr<Entity> e = move(inside[name]);
-	inside.erase(name);
-	return move(e);
+	if (inside.count(name)) {
+		std::shared_ptr<Entity> e = move(inside[name]);
+		inside.erase(name);
+		return move(e);
+	}
+	else {
+		std::shared_ptr<Entity> e;
+		for (const auto& x : inside) {
+			e = x.second->remove(name);
+			if (e) break;
+		}
+		return move(e);
+	}
+	
 }
 
 void Entity::open() {
--- escaperoom/src/main.cpp
+++ escaperoom/src/main.cpp
@@ -11,9 +11,15 @@ int main() {
 	SetConsoleOutputCP(65001); // set output console codepage to utf8
 #endif
 	std::shared_ptr<Entity> table = std::make_unique<Entity>("table", "table");
-	std::shared_ptr<Entity> cabinet = std::make_unique<Entity>("cabinet", "cabinet");
-	table->add(std::make_unique<Entity>("spoon", "spoon"));
-	table->add(std::make_unique<Entity>("fork", "fork"));
-	cabinet->add(table->remove("spoon"));
+	std::shared_ptr<Entity> stash = std::make_unique<Entity>("stash", "stash");
+	std::shared_ptr<Entity> jeans = std::make_unique<Entity>("jeans", "jeans");
+	std::shared_ptr<Entity> pocket = std::make_unique<Entity>("pocket", "pocket");
+	std::shared_ptr<Entity> coin = std::make_unique<Entity>("coin", "coin");
+	std::shared_ptr<Entity> money = std::make_unique<Entity>("money", "money");
+	table->add(stash);
+	stash->add(money);
+	table->add(jeans);
+	jeans->add(pocket);
+	pocket->add(coin);
 	return 0;
 }
