95b03d8d83d69f72b86f7576634bbb8f1f0fbf51
Máté Molnár <mmateka89@gmail.com>
2021-01-18 12:32:43 +0100

    Remove default constructor for Entity, add name property

--- escaperoom/src/Entity.h
+++ escaperoom/src/Entity.h
@@ -5,14 +5,14 @@
 class Entity
 {
 private:
+	std::string _name;
 	std::string _description;
 
 public:
 	std::unordered_set<std::unique_ptr<Entity>> inside;
 
 public:
-	Entity() : _description("Ennek a tárgynak nincs leírása...") {};
-	Entity(const std::string_view description) : _description(description) {};
+	Entity(const std::string_view name, const std::string_view description) : _name{ name }, _description{ description } {};
 	void check();
 	void add(std::unique_ptr<Entity>& e);
 
