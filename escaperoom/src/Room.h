a7ef1955e9aa231232abedbf2be6a671682d7ff5
Máté Molnár <mmateka89@gmail.com>
2021-01-18 14:43:18 +0100

    Add "addExit" function to Room class

--- escaperoom/src/Room.h
+++ escaperoom/src/Room.h
@@ -1,7 +1,22 @@
 #pragma once
+#include <cassert>
 #include "Entity.h"
 class Room :
     public Entity
 {
+
+public:
+  enum class Direction {
+    NORTH, EAST, SOUTH, WEST
+  };
+
+private:
+  std::unordered_map<Direction, std::shared_ptr<Room>> _exits;
+
+public:
+  void addExit(const Direction direction, std::shared_ptr<Room> exit) {
+    assert(_exits.count(direction) != 0);
+    _exits[direction] = move(exit);
+  }
 };
 
