a1538e8f44542062654877aace7b345903874d78
Máté Molnár <mmateka89@gmail.com>
2021-01-20 13:50:30 +0100

    Add pickup function for player, add support for multiword actions

--- escaperoom/src/Player.cpp
+++ escaperoom/src/Player.cpp
@@ -25,7 +25,21 @@ void Player::_check(const std::vector<std::string>& items) const {
 	else {
 		auto result{ _currentRoom->getItem(items[0]) };
 		if (result.has_value()) {
-			result->get()->check();
+			result.value()->check();
+		}
+		else {
+			std::cout << "Nem látok ilyen tárgyat.";
+		}
+	}
+}
+
+void Player::_pickup(const std::vector<std::string>& items) {
+	if (items.empty()) std::cout << "Mit vegyek fel?";
+	else if (items.size() > 1) std::cout << "Egyszerre csak egy tárgyat tudok felvenni.";
+	else {
+		auto result{ _currentRoom->removeItem(items[0]) };
+		if (result.has_value()) {
+			_backpack.addItem(result.value());
 		}
 		else {
 			std::cout << "Nem látok ilyen tárgyat.";
@@ -40,10 +54,21 @@ void Player::input() {
 	std::getline(std::cin, inputText);
 	auto splitText{ split(inputText) };
 	if (!splitText.empty()) {
-		const auto action{ splitText[0] };
+		auto actionLen{ 1 };
+		auto action{ splitText[0] };
 		std::vector<std::string> items;
 		if (splitText.size() > 1) {
-			items = std::vector<std::string>(splitText.begin() + 1, splitText.end());
+			auto multiwordAction{splitText[0]};
+			auto multiwordActionLen{ 1 };
+			for (auto i = 1; i < splitText.size(); i++) { // multiword action support
+				multiwordActionLen++;
+				multiwordAction += " " + splitText[i];
+				if (_actions.count(multiwordAction)) {
+					action = multiwordAction;
+					actionLen = multiwordActionLen;
+				}
+			}
+			items = std::vector<std::string>(splitText.begin() + actionLen, splitText.end());
 		}
 		if (_actions.count(action)) {
 			_actions[action](items);
--- escaperoom/src/Player.h
+++ escaperoom/src/Player.h
@@ -10,10 +10,12 @@ private:
 	StorageItem _backpack{"backpack", "backpack"};
 	std::unordered_map<std::string,
 		std::function<void(const std::vector<std::string>& items)>> _actions{
-		{"nézd", [this] (const std::vector<std::string>& items){this->_check(items); }}
+		{"nézd", [this] (const std::vector<std::string>& items){this->_check(items); }},
+		{"vedd fel", [this](const std::vector<std::string>& items) {this->_pickup(items); }}
 	};
 
 	void _check(const std::vector<std::string>& items) const;
+	void _pickup(const std::vector<std::string>& items);
 
 public:
 	explicit Player(std::shared_ptr<Room> currentRoom);
